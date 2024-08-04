80fdaddc1ec829a2d73160947d792f3a0a4585ef
Catalina <catalina.salazar@estudiantec.cr>
2024-08-03 19:16:12 -0600

    Código con errores de formato

---
+++ Tarea 1/ejemplo_errores_Flake8.py
@@ -0,0 +1,21 @@
+#Trabajo extra clase semana 4
+#Hacer un programa que genere la serie de Fibonacci
+#hasta cierta cantidad de veces. Retorna una lista.
+
+
+#Genera la serie de Fibonacci n veces
+def fibonacci_series(n):
+    series = []
+    a, b = 0, 1
+    while len(series)<n:
+        
+        series.append(a)
+        a, b = b, a + b
+        
+    return series
+
+#Ejemplo
+
+
+n = 10
+print(f"Los primeros {n} números de la serie de Fibonacci son: {fibonacci_series(n)}")
